d559d5f4cd3e566b2154b46286732ad2d6f9a23c
Lorne Markham <lorne@openhouseconcept.com>
2014-03-19 13:42:55 -0700

    nothing

--- wp-config.php
+++ wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'brownlee');
 
 /** MySQL database username */
-define('DB_USER', 'bladmin');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'blpassword');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
@@ -50,8 +50,8 @@ define('AUTH_SALT',        'put your unique phrase here');
 define('SECURE_AUTH_SALT', 'put your unique phrase here');
 define('LOGGED_IN_SALT',   'put your unique phrase here');
 define('NONCE_SALT',       'put your unique phrase here');
-define('WP_HOME','http://localhost/brownlee-dev');
-define('WP_SITEURL','http://localhost/brownlee-dev');
+define('WP_HOME','http://localhost');
+define('WP_SITEURL','http://localhost');
 
 /**#@-*/
 
